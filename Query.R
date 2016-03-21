6a787aba90aed099eb84c3fac9555d082c7057e6
adamliu84 <adam.liu84@gmail.com>
2016-03-22 00:18:28 +0800

    First commit with code to crawl STI component from Yahoo finance

---
+++ Query.R
@@ -0,0 +1,10 @@
+source("Function.R")
+
+# Get the required data
+dfTag <- generateTagRow()
+
+# Yahoo finance csv download url
+szURL <- paste0("http://download.finance.yahoo.com/d/quotes.csv?s=",paste(listSymbols(),collapse=","),"&f=", paste(dfTag$tag, collapse=""),"&e=.csv")
+dfData <- read.csv(szURL,header=FALSE)
+names(dfData) <- dfTag$description
+print(dfData)
