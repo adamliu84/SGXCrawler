9f2791b474726975b6b37a72c5857c0e29c4a7e2
adamliu84 <adam.liu84@gmail.com>
2016-04-04 00:37:42 +0800

    CSV writing

--- isinsymbol.hs
+++ isinsymbol.hs
@@ -2,19 +2,28 @@ import Network.HTTP.Conduit
 import Network.HTTP
 import qualified Data.ByteString.Lazy as LB
 import Data.Char (chr)
-import Data.List (isInfixOf)
-import Data.String.Utils (split, replace)
-import qualified Data.ByteString as B
+import Data.String.Utils (split)
+
+csvfilename = "fulllisting.csv"
 
 --http://www.sgx.com/wps/portal/sgxweb/home/company_disclosure/isin_code_download
 url = "http://infopub.sgx.com/FileOpen/.ashx?App=ISINCode&FileID=1"
+
 
 downloadPage ::  IO [String]
 downloadPage = do 
                   return.lines.bsToStr =<< simpleHttp url
 		  where bsToStr = map (chr . fromEnum).LB.unpack
 
+writeCSVHeader = appendFile csvfilename "Name;Status;ISINCode;Code;ShortName"
+writeCSVData x = appendFile csvfilename x
+
 main = do
+	-- Parse sgx page listing
 	lSym <- downloadPage
-	mapM_ print lSym
-	print "SDF"
+
+	-- Write csv file
+	writeCSVHeader
+	mapM_ writeCSVData lSym
+
+	print "FIN"
